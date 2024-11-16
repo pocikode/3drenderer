a3e982781e4c12c57040b276f6b4279ce5436bf8
Agus Supriyatna <agus@motionpay.id>
2024-11-17 00:01:59 +0700

    Vector and Points - add 9x9x9 vec3 points of cube

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "display.h"
+#include "vector.h"
+#define N_POINTS (9 * 9 * 9)
+
+vec3_t cube_points[N_POINTS];
 
 bool is_running = false;
 
@@ -19,6 +23,16 @@ void setup(void) {
         window_width,
         window_height
     );
+
+    int point_count = 0;
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y++) {
+            for (float z = -1; z <= 1; z++) {
+                vec3_t new_point = {.x = x, .y = y, .z = z};
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
 }
 
 void process_input(void) {
