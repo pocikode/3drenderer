351158c5a3516b47af21615c997948f3b461d964
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2025-10-17 17:12:11 +0700

    rotate cube

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
 vec3_t camera_position = {0, 0, -5};
+vec3_t cube_rotation = {0, 0, 0};
 
 float fov_factor = 640;
 
@@ -55,7 +56,7 @@ void process_input(void)
   }
 };
 
-// Orthographic Projection
+// Perspective Projection
 vec2_t project(vec3_t point)
 {
   vec2_t projected_point = {
@@ -67,13 +68,23 @@ vec2_t project(vec3_t point)
 
 void update(void)
 {
+  cube_rotation.x += 0.01;
+  cube_rotation.y += 0.01;
+  cube_rotation.z += 0.01;
+
   for (int i = 0; i < N_POINTS; i++)
   {
     vec3_t point = cube_points[i];
 
-    point.z -= camera_position.z;
+    // rotate
+    vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+    transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+    transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
 
-    vec2_t projected_point = project(point);
+    // translate the point away from the camera
+    transformed_point.z -= camera_position.z;
+
+    vec2_t projected_point = project(transformed_point);
     projected_points[i] = projected_point;
   }
 };
