e8d0504800251ca2322197deeb83f87a9c898cc0
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2025-10-21 17:11:26 +0700

    colored triangle faces

--- src/main.c
+++ src/main.c
@@ -29,7 +29,8 @@ void setup(void)
     window_width,
     window_height);
 
-  load_obj_file_data("../assets/cube.obj");
+  load_cube_mesh_data();
+  // load_obj_file_data("../assets/cube.obj");
 }
 
 void process_input(void)
@@ -146,15 +147,22 @@ void update(void)
     }
 
     // perform projection
-    triangle_t projected_triangle;
+    vec2_t projected_points[3];
     for (int j = 0; j < 3; j++)
     {
-      vec2_t projected_point = project(transformed_vertices[j]);
-      projected_point.x += (int)(window_width / 2);
-      projected_point.y += (int)(window_height / 2);
-      projected_triangle.points[j] = projected_point;
+      projected_points[j] = project(transformed_vertices[j]);
+      projected_points[j].x += (int)(window_width / 2);
+      projected_points[j].y += (int)(window_height / 2);
     }
 
+    triangle_t projected_triangle = {
+      .points = {
+        {projected_points[0].x, projected_points[0].y},
+        {projected_points[1].x, projected_points[1].y},
+        {projected_points[2].x, projected_points[2].y},
+      },
+      .color = mesh_face.color,
+    };
     array_push(triangles_to_render, projected_triangle);
   }
 };
@@ -186,7 +194,7 @@ void render(void)
         triangle.points[1].y,
         triangle.points[2].x,
         triangle.points[2].y,
-        0xFF808080);
+        triangle.color);
     }
 
     // draw wireframe
